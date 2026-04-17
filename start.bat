9580b474f5fed41df583061300484da53d8180d5
Smile <Maksimgem2009max@gmail.com>
2026-04-17 11:27:20 +0300

    update start.bat to include npm install for front and back services

--- start.bat
+++ start.bat
@@ -1,7 +1,7 @@
 @echo off
 
 :: Открываем фронт
-start cmd /k "cd /d %~dp0front && npm run dev"
+start cmd /k "cd /d %~dp0front && npm install && npm run dev"
 
 :: Открываем бэк
-start cmd /k "cd /d %~dp0back && npm start"
+start cmd /k "cd /d %~dp0back && npm install && npm start"
