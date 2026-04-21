4e1bf278a5ea50941f148b4ad0e96b52882d80f9
Smile <Maksimgem2009max@gmail.com>
2026-04-21 10:50:33 +0300

    Add SVG icons for Playlist and Your Playlists
    - Created Playlist.svg with a design representing a playlist.
    - Created YourPlayLists.svg featuring a list layout for user playlists.

--- front/src/components/menu.jsx
+++ front/src/components/menu.jsx
@@ -6,21 +6,55 @@ const sidebarStyle = {
   backgroundColor: "rgba(13, 13, 13, 0.85)",
   color: "#fff",
   padding: "20px",
-  oppacity: "75%",
+  position: "fixed",
+  top: 0,
+  left: 0
+
 };
 
+const playlistStyle = {
+  width: "100%",
+  marginTop: "10px",
+  padding: "10px",
+  
+
+};
 function Sidebar() {
   return (
     <div style={sidebarStyle}>
       <h2>Меню</h2>
       <div style={{ borderRadius: "20%" }}>
-        <img src="../assets/imgs/Home.svg" alt="Home" />
+        <img src="../assets/imgs/Home.svg" alt="Home" style={{ width: "24px", height: "24px" }} />
         <h3>Головна</h3>
       </div>
-      <img src="../assets/imgs/MyMediatek.svg" alt="Library" />
+      <img src="../assets/imgs/MyMediatek.svg" alt="Library" style={{ width: "24px", height: "24px" }} />
       <h3>Моя медіатека</h3>
-      
-      </div>
+      <div style={{ padding: "5px", border: "1px solid #426D83" }}></div>
+      <h2>Плейлисти</h2>
+      <img src="../assets/imgs/Heart.svg" alt="Heart" style={{ width: "24px", height: "24px" }} />
+      <h3>Улюблені треки</h3>
+      <img src="../assets/imgs/Playlist.svg" alt="Playlist" style={{ width: "24px", height: "24px" }} />
+      <h3>Створити плейлист</h3>
+      <h3>Ваші плейлисти</h3>
+      <img src="../assets/imgs/YourPlayLists.svg" alt="" style={{ width: "24px", height: "24px" }} />
+        <div style={playlistStyle}>
+          <img src="" alt="" />
+          <h4>Плейлист 1</h4>
+          <p>Опис плейлиста 1</p>
+          <img src="" alt="" />
+          <h4>Плейлист 2</h4>
+          <p>Опис плейлиста 2</p>
+          <img src="" alt="" />
+          <h4>Плейлист 3</h4>
+          <p>Опис плейлиста 3</p>
+          <img src="" alt="" />
+          <h4>Плейлист 4</h4>
+          <p>Опис плейлиста 4</p>
+          <img src="" alt="" />
+          <h4>Плейлист 5</h4>
+          <p>Опис плейлиста 5</p>
+        </div>
+    </div>
   );
 }
 
