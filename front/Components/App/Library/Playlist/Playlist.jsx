09ac5b66fadeca7360d12ff1b54010fa712f4310
stealbe <paradovskiialex@gmail.com>
2026-04-29 11:23:32 +0300

    Refactor app structure: move App component to Main, add Playlist component, and update styles

---
+++ front/Components/App/Library/Playlist/Playlist.jsx
@@ -0,0 +1,36 @@
+import "./Playlist.css";
+import { MusicSCard } from "../../../Cards";
+
+export default function Playlist({ name, icon, author, tracks = [] }) {
+    return (
+        <div className="playlist">
+            <div className="playlist-header" style={{ backgroundImage: `url(${icon})` }}>
+                <p>Плейлист</p>
+                <h3>{name}</h3>
+                <div className="author">
+                    <img src={author.icon} alt={author} />
+                    <span>{author.name}</span>
+                    <br />
+                    <span>{tracks.length} треків</span>
+                </div>
+            </div>
+            <div className="controls">
+                <button onClick={() => { }}>Play</button>
+                <button onClick={() => { }}>Random</button>
+                <button onClick={() => { }}>Download</button>
+            </div>
+            <div className="playlist-tracks">
+                <div className="playlist-tracks-header">
+                    <span>Альбом</span>
+                    <span>Дата додавання</span>
+                    <span>час</span>
+                </div>
+                <div className="playlist-tracks-list">
+                    {tracks.map((track, index) => (
+                        <MusicSCard key={index} {...track} />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
--- front/main.jsx
+++ front/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
+import Main from "./Components/App/Main";
+import Playlist from "./Components/App/Library/Playlist/Playlist";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    {/* <Main /> */}
+    <Playlist
+      name="Мій плейлист"
+      icon="https://i.pinimg.com/564x/1c/8e/0b/1c8e0b9a7d2f5a3c9e4b6c9e5f1a2b.jpg"
+      author={{ name: "Автор плейлиста", icon: "https://i.pinimg.com/564x/1c/8e/0b/1c8e0b9a7d2f5a3c9e4b6c9e5f1a2b.jpg" }}
+      tracks={[
+        {
+          id, num, icon, title, artists, alboum, listenCount, duration, addDate
+          
+        }
+      ]}
+    />
   </StrictMode>
 );
